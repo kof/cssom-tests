48f09ccfe87acd85b101dbbe292389030941e30b
Oleg Isonen <oleg008@gmail.com>
2018-09-21 00:10:40 +0200

    predefined changes, render more, clear

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import jss from 'jss';
 
 const getRandomColor = () => `#${Math.floor(Math.random() * 0x1000000).toString(16)}`;
 
+const changes = new Array(1e6).fill(null).map(getRandomColor);
+
 const createStyles = amount =>
   new Array(amount).fill(null).reduce(
     (styles, style, i) => ({
@@ -18,29 +20,65 @@ const createStyles = amount =>
     {}
   );
 
-const createElements = classes =>
-  Object.values(classes).map(className => {
+const createElements = classNames =>
+  classNames.map(className => {
     const el = document.createElement('div');
     el.className = className;
     return el;
   });
 
 const animate = sheet => {
+  let stop;
+
   (function run() {
     for (const name in sheet.classes) {
-      sheet.getRule(name).prop('background', getRandomColor());
+      if (changes.length === 0) break;
+      sheet.getRule(name).prop('background', changes.pop());
     }
-    requestAnimationFrame(run);
+    if (!stop && changes.length !== 0) requestAnimationFrame(run);
   })();
+
+  return () => {
+    stop = true;
+  };
 };
 
-const render = () => {
-  const sheet = jss.createStyleSheet(createStyles(400), {link: true}).attach();
-  const elements = createElements(sheet.classes);
-  const fragment = document.createDocumentFragment();
-  elements.forEach(fragment.appendChild, fragment);
-  document.body.appendChild(fragment);
-  animate(sheet);
+const renderObjects = amount => {
+  const sheet = jss.createStyleSheet(createStyles(amount), {link: true}).attach();
+  const elements = createElements(Object.values(sheet.classes));
+  const container = document.createElement('div');
+  elements.forEach(container.appendChild, container);
+  document.body.appendChild(container);
+  const stop = animate(sheet);
+
+  return () => {
+    stop();
+    document.body.removeChild(container);
+    sheet.detach();
+  };
 };
 
-render();
+const renderControls = () => {
+  let remove;
+  let amount = 100;
+
+  const buttonRender = document.createElement('button');
+  buttonRender.innerText = `Render ${amount}`;
+  buttonRender.onclick = () => {
+    if (remove) remove();
+    remove = renderObjects(amount);
+    amount += 100;
+    buttonRender.innerText = `Render ${amount}`;
+  };
+  document.body.appendChild(buttonRender);
+
+  const buttonClear = document.createElement('button');
+  buttonClear.innerText = 'Clear';
+  buttonClear.onclick = () => {
+    remove();
+    remove = null;
+  };
+  document.body.appendChild(buttonClear);
+};
+
+renderControls();
