1c06d01730ee68fd4ba0a9b78f0bf4cac0e7e177
Oleg Isonen <oleg008@gmail.com>
2018-09-27 11:00:07 +0200

    immutable scenario, full update each time

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import jss from 'jss';
 import {render as renderControls} from './controls';
+import {Map} from 'immutable';
 
 const getRandomColor = () => `#${Math.floor(Math.random() * 0x1000000).toString(16)}`;
-
-const changes = new Array(1e5).fill(null).map(getRandomColor);
 
 const createStyles = amount =>
   new Array(amount).fill(null).reduce(
@@ -28,42 +27,86 @@ const createElements = classNames =>
     return el;
   });
 
-const animate = sheet => {
-  let stop;
-  let index = 0;
-
-  const next = () => {
-    const change = changes[index];
-    if (changes.length > index) index++;
-    else index = 0;
-    return change;
-  };
-
-  (function run() {
-    for (const name in sheet.classes) {
-      sheet.getRule(name).prop('background-color', next());
-    }
-    if (!stop) requestAnimationFrame(run);
-  })();
-
-  return () => {
-    stop = true;
-  };
-};
-
 const renderObjects = amount => {
   const sheet = jss.createStyleSheet(createStyles(amount), {link: true}).attach();
   const elements = createElements(Object.values(sheet.classes));
   const container = document.createElement('div');
   elements.forEach(container.appendChild, container);
   document.body.appendChild(container);
-  const stop = animate(sheet);
 
-  return () => {
-    stop();
+  const remove = () => {
     document.body.removeChild(container);
     sheet.detach();
   };
+
+  const update = data => {
+    // [{rule: 'box-0': color: 'red'}]
+    data.forEach(value => {
+      let {rule, color} = value;
+      if ('get' in value) {
+        rule = value.get('rule');
+        color = value.get('color');
+      }
+      sheet.getRule(rule).prop('background-color', color);
+    });
+  };
+
+  return {remove, update};
 };
 
-renderControls(renderObjects);
+const subscribe = (samples, callback) => {
+  let subscribed = true;
+  let index = 0;
+
+  (function run() {
+    if (!subscribed) return;
+    callback(samples[index]);
+    index = samples[index + 1] ? index + 1 : 0;
+    requestAnimationFrame(run);
+  })();
+
+  return () => {
+    subscribed = false;
+  };
+};
+
+const generateSamplesPlain = amount =>
+  // [[{rule: 'box-0': color: 'red'}], [{rule: 'box-0': color: 'red'}]]
+  new Array(1e3).fill(null).map(() =>
+    new Array(amount).fill(null).map((v, index) => ({
+      rule: `box-${index}`,
+      color: getRandomColor()
+    }))
+  );
+
+renderControls(amount => {
+  const {remove, update} = renderObjects(amount);
+  const samples = generateSamplesPlain(amount);
+  const unsubscribe = subscribe(samples, update);
+  return () => {
+    unsubscribe();
+    remove();
+  };
+});
+
+const generateSamplesImmutable = amount =>
+  // [[{rule: 'box-0': color: 'red'}], [{rule: 'box-0': color: 'red'}]]
+  new Array(1e3).fill(null).map(() =>
+    new Array(amount).fill(null).map(
+      (v, index) =>
+        new Map({
+          rule: `box-${index}`,
+          color: getRandomColor()
+        })
+    )
+  );
+
+renderControls(amount => {
+  const {remove, update} = renderObjects(amount);
+  const samples = generateSamplesImmutable(amount);
+  const unsubscribe = subscribe(samples, update);
+  return () => {
+    unsubscribe();
+    remove();
+  };
+});
